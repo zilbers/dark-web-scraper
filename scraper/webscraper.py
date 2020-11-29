adb6fcea4adaa03b4be82e8c7fb9d0b84cc16821
Omri Zilberstein <67857576+zilbers@users.noreply.github.com>
2020-11-29 18:26:28 +0200

    Added function to combine all csv's

--- scraper/webscraper.py
+++ scraper/webscraper.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+import glob
 import pandas as pd
 import time
 
@@ -29,32 +30,63 @@ def set_profile():
     return profile
 
 
+def forum_scrape():
+    all_csv = glob.glob("data\\seperated\\*.csv")
+    frames = []
+
+    for path in all_csv:
+        frames.append(pd.read_csv(path))
+
+    result = pd.concat(frames, ignore_index = True)
+    result = result.drop_duplicates(subset="content")
+    result.to_csv('data/ForumScrape.csv')
+
+
 def main():
+    print("\nSetting up your anonymous profile to browse the dark web!")
     profile = set_profile()
 
     # Setting the driver
+    print("Setting up webdriver with Proxy..")
     driver = webdriver.Firefox(executable_path=PATH, firefox_profile=profile)
 
-    data = {"headers": [], "content": [], "author": []}
+    data = {"headers": [], "content": [], "author": [], "date": []}
 
     for search in KEYWORDS:
         # Scrapping
+        print(f"\nSearching: {search}..")
         driver.get(f'{URL}search?q={search}')
 
         headers = driver.find_elements_by_tag_name('h4')
         content = driver.find_elements_by_class_name('text')
         author = driver.find_elements_by_class_name("col-sm-6")
 
+        if len(headers) == 0:
+            print(f"\tNo data at: {search}!")
+            continue
+
+        print(f"\tAdding data to file..")
         for index in range(len(headers)):
-            data['headers'].append(headers[index].text)
-            data['content'].append(content[index].text)
-            data['author'].append(author[index].text)
+            author_date = author[index * 2].text.split("at")
 
-    df = pd.DataFrame(data, columns=['headers', 'content', 'author', ...])
-    df.to_csv(f'data/scrape-{current_milli_time()}.csv')
+            data['headers'].append(headers[index].text.strip())
+            data['content'].append(content[index].text.strip())
+            data['author'].append(author_date[0].strip())
+            data['date'].append(author_date[1].strip())
 
+    print('Closing broswer')
     driver.close()
     driver.quit()
+
+    date = current_milli_time()
+    print(f"Creating new csv file: scrape-{date}")
+    df = pd.DataFrame(
+        data, columns=['headers', 'content', 'author', 'date', ...])
+    df.to_csv(f'data/seperated/scrape-{date}.csv')
+
+    print("Combining all CSV files to one...")
+    forum_scrape()
+
 
 
 if __name__ == "__main__":
